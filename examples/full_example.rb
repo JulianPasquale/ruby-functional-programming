620344a6aa08cca4b4d48538a2bfc05bb7636946
Julian Pasquale <julianagustin78@gmail.com>
2022-09-23 14:17:05 -0300

    Implement recursive enum

--- examples/full_example.rb
+++ examples/full_example.rb
@@ -12,12 +12,26 @@ starwars_characters_api = api_handler.curry['https://swapi.dev/api/people']
 
 paginated_request = (starwars_characters_api << pagination_options) >> parse_response
 
+process_page = -> y, page_number = 1 do
+  result = paginated_request.call(page_number)
+
+  result[:results].each do |elem|
+    puts "Processed #{elem[:name]}"
+    y << elem
+  end
+
+  return unless result[:next]
+
+  process_page.call(y, page_number + 1)
+end
+
 enum = Enumerator.new do |y|
   current_page = 1
   result = paginated_request.call(current_page)
   
   loop do
     result[:results].each do |elem|
+      puts "Processed #{elem[:name]}"
       y << elem
     end
 
@@ -28,6 +42,11 @@ enum = Enumerator.new do |y|
   end
 end
 
+enum2 = Enumerator.new do |y|
+  process_page.call(y)
+end
+
 byebug
 
 enum.first
+enum.lazy.drop(10).map { |character| character[:name] }.first(10)
